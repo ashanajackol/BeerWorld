cdf730b0acad0de749e05b5f2dd60bca30289907
ashana727 <a.uthpala@infoplaza.nl>
2023-04-14 21:35:09 +0530

    fixing rebase issues

--- app/src/main/java/com/ashana/caching/beerworld/presentation/BeerScreen.kt
+++ app/src/main/java/com/ashana/caching/beerworld/presentation/BeerScreen.kt
@@ -20,13 +20,13 @@ import com.ashana.caching.beerworld.data.remote.domain.Beer
 
 @Composable
 fun BeerScreen(
-    beer: LazyPagingItems<Beer>
+    beers: LazyPagingItems<Beer>
 ) {
     val context = LocalContext.current
-    LaunchedEffect(key1 = beer.loadState) {
-        if (beer.loadState.refresh is LoadState.Error) {
+    LaunchedEffect(key1 = beers.loadState) {
+        if (beers.loadState.refresh is LoadState.Error) {
             Toast.makeText(
-                context, "Error! ${(beer.loadState.refresh as LoadState.Error).error.message}",
+                context, "Error! ${(beers.loadState.refresh as LoadState.Error).error.message}",
                 Toast.LENGTH_LONG
             ).show()
         }
